d9fd4048fc2ad1e43d3f46363b79e11c6c721fc0
unknown <kunalkeshri047@gmail.com>
2020-07-02 03:09:31 +0530

    Cricket Game Task

---
+++ cricket.js
@@ -0,0 +1,161 @@
+var Team = /** @class */ (function () {
+    function Team(TeamList, team_name) {
+        var _this = this;
+        this.i = 0;
+        this.j = 0;
+        this.start = false;
+        this.stop = false;
+        this.Team_total = 0;
+        this.once = true;
+        this.top_scorer = { run: 0, Player_Id: "", total: 0 };
+        this.start_playing = function () {
+            setTimeout(function () {
+                console.log(3);
+                setTimeout(function () {
+                    console.log(2);
+                    setTimeout(function () {
+                        console.log(1);
+                        setTimeout(function () {
+                            _this.start = true;
+                            _this.stop = true;
+                            console.log("Match started");
+                            var seconds = 60;
+                            console.log("outer", seconds);
+                            var x = function () {
+                                if (!(_this.start) || seconds < 0) {
+                                    document.getElementById("timer").innerHTML = "";
+                                    return;
+                                }
+                                console.log(seconds);
+                                document.getElementById("timer").innerHTML = seconds.toString() + "s";
+                                setTimeout(function () {
+                                    seconds--;
+                                    x();
+                                }, 1000);
+                            };
+                            x();
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+            setTimeout(function () {
+                _this.start = false;
+            }, 60000);
+        };
+        this.get_runs = function () {
+            var table_val = document.getElementById(_this.team_name);
+            var col = 7;
+            if (_this.i < 10 && _this.start) {
+                var runs = Math.floor(Math.random() * (7 - 0));
+                _this.TeamList[_this.i].player.push(runs);
+                table_val.rows[_this.i + 1].cells[Number(_this.j) + 1].innerHTML = runs.toString();
+                console.log("Player ", _this.i + 1, " Ball ", _this.j + 1, " = ", _this.TeamList[_this.i].player[_this.j], "Runs");
+                if (_this.j == 5 || _this.TeamList[_this.i].player[_this.j] == 0) {
+                    console.log(" Total Runs = ", _this.TeamList[_this.i].total_run);
+                    _this.TeamList[_this.i].total_run += _this.TeamList[_this.i].player[_this.j];
+                    if (_this.top_scorer.run < _this.TeamList[_this.i].total_run) {
+                        _this.top_scorer.run = Math.max(_this.top_scorer.run, _this.TeamList[_this.i].total_run);
+                        _this.top_scorer.Player_Id = "Player ".concat((_this.i + 1).toString());
+                    }
+                    _this.i++;
+                    _this.j = 0;
+                }
+                else {
+                    _this.Team_total += _this.TeamList[_this.i].player[_this.j];
+                    _this.TeamList[_this.i].total_run += _this.TeamList[_this.i].player[_this.j];
+                    table_val.rows[_this.i + 1].cells[col].innerHTML = (_this.TeamList[_this.i].total_run).toString();
+                    console.log("  Total Runs = ", _this.TeamList[_this.i].total_run);
+                    _this.j++;
+                }
+            }
+            else {
+                if (!_this.stop && !_this.start) {
+                    alert("Match will start in 3 seconds ........");
+                }
+                else {
+                    _this.start = false;
+                    alert("Match is Over");
+                }
+            }
+            _this.team_total();
+        };
+        this.display_runs = function () {
+            _this.TeamList.forEach(function (element) {
+                element.player.forEach(function (element2) {
+                    console.log(element2);
+                });
+            });
+        };
+        this.team_total = function () {
+            if (_this.team_name == "team1")
+                document.getElementById("score1").innerHTML = _this.Team_total.toString();
+            else if (_this.team_name == "team2")
+                document.getElementById("score2").innerHTML = _this.Team_total.toString();
+            console.log(_this.Team_total);
+            _this.top_scorer.total = _this.Team_total;
+            console.log(_this.top_scorer);
+        };
+        this.TeamList = TeamList;
+        this.team_name = team_name;
+    }
+    return Team;
+}());
+var Team1 = new Team([{ player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+], "team1");
+var Team2 = new Team([{ player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+], "team2");
+var team1_total = Team1.top_scorer;
+var team2_total = Team2.top_scorer;
+var maxTeam1 = Team1.top_scorer;
+var maxTeam2 = Team2.top_scorer;
+var generate_result = function () {
+    var px = true;
+    console.log(team2_total.total);
+    if (Number(team1_total.total) > Number(team2_total.total) && team1_total.total != 0 && team2_total.total != 0)
+        document.getElementById("won").innerHTML = "MATCH WON BY TEAM1 ";
+    else if (Number(team1_total.total) < Number(team2_total.total) && team1_total.total != 0 && team2_total.total != 0)
+        document.getElementById("won").innerHTML = "MATCH WON BY TEAM2 ";
+    else if (Number(team1_total.total) == Number(team2_total.total) && team1_total.total != 0)
+        document.getElementById("won").innerHTML = "MATCH DRAW ";
+    else {
+        document.getElementById("won").innerHTML = "BOTH TEAM HAS NOT PLAYED ";
+        px = false;
+    }
+    if (!px)
+        document.getElementById("mom").innerHTML = "";
+    else {
+        if (Number(maxTeam1.run) > Number(maxTeam2.run)) {
+            document.getElementById("mom").innerHTML = "Man of the Match :"
+                + maxTeam1.Player_Id +
+                " Team 1 Score:"
+                + maxTeam1.run;
+        }
+        else if (Number(maxTeam1.run) < Number(maxTeam2.run)) {
+            document.getElementById("mom").innerHTML = "Man of the Match : " + maxTeam2.Player_Id + " Team 2 Score:" + maxTeam2.run;
+        }
+        else {
+            document.getElementById("mom").innerHTML = "Man of the Match : " + maxTeam1.Player_Id + " Team 1 Score:" + maxTeam1.run;
+            document.getElementById("mom").innerHTML = "Man of the Match : " + maxTeam2.Player_Id + " Team 1 Score:" + maxTeam2.run;
+        }
+    }
+};
---
+++ cricket.ts
@@ -0,0 +1,209 @@
+interface over_runs {
+    [index: number]: Number;
+}
+interface Team_Details {
+    player: over_runs;
+    total_run: Number;
+}
+class Team {
+    TeamList;
+    i = 0;
+    j = 0;
+    start=false;
+    stop=false;
+    Team_total = 0;
+    once=true;
+    top_scorer={run:0,Player_Id:"",total:0};
+    team_name;
+    constructor(TeamList: Team_Details[],team_name:string) {
+        this.TeamList = TeamList;
+        this.team_name=team_name;
+    }
+
+
+
+    start_playing = () => {
+        setTimeout(() => {
+            console.log(3)
+            setTimeout(() => {
+                console.log(2)
+                setTimeout(() => {
+                    console.log(1);
+                    setTimeout(() => {
+                        this.start=true;
+                        this.stop=true;
+                        console.log("Match started");
+                         let seconds=60;
+                         console.log("outer",seconds);
+                            let x = () => {
+                            if (!(this.start) || seconds < 0)
+                            {
+                            document.getElementById("timer").innerHTML = "";
+                            return;
+                            }
+                                console.log(seconds);
+                                document.getElementById("timer").innerHTML = seconds.toString() + "s";
+                                setTimeout(function () {
+                                 seconds--;
+                                 x();
+                             }, 1000);
+                         }
+                         x();
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+        setTimeout(() => {
+            this.start=false;
+        }, 60000);
+    };
+
+
+
+
+    get_runs = () => {
+        let table_val=<HTMLTableElement>document.getElementById(this.team_name);
+        let col=7;
+        if (this.i < 10 && this.start) {
+            let runs = Math.floor(Math.random() * (7 - 0));
+            this.TeamList[this.i].player.push(runs);
+            table_val.rows[this.i+1].cells[Number(this.j)+1].innerHTML=runs.toString();
+            console.log("Player ", this.i + 1, " Ball ", this.j + 1, " = ", this.TeamList[this.i].player[this.j], "Runs");
+
+            if (this.j == 5 || this.TeamList[this.i].player[this.j] == 0) {
+                
+                console.log(" Total Runs = ",this.TeamList[this.i].total_run);
+                this.TeamList[this.i].total_run+=this.TeamList[this.i].player[this.j];
+                if(this.top_scorer.run<this.TeamList[this.i].total_run)
+                {
+                this.top_scorer.run=Math.max(this.top_scorer.run,this.TeamList[this.i].total_run);
+                this.top_scorer.Player_Id="Player ".concat((this.i+1).toString());
+                }
+                this.i++;
+                this.j = 0;
+            }
+            else {
+                this.Team_total+=this.TeamList[this.i].player[this.j];
+                this.TeamList[this.i].total_run+=this.TeamList[this.i].player[this.j];
+                table_val.rows[this.i+1].cells[col].innerHTML=(this.TeamList[this.i].total_run).toString();
+                console.log("  Total Runs = ",this.TeamList[this.i].total_run);
+                this.j++;
+            }
+        }
+        else{
+            if(!this.stop && !this.start)
+            {
+                alert("Match will start in 3 seconds ........")
+            }
+            else
+            {
+            this.start=false;
+            alert("Match is Over");
+            }
+        }
+        this.team_total();
+
+    }
+
+
+    display_runs = () => {
+        this.TeamList.forEach(element => {
+            element.player.forEach(element2 => {
+                console.log(element2);
+            });
+
+        });
+    }
+
+    team_total=()=>{
+
+        if(this.team_name=="team1")
+        document.getElementById("score1").innerHTML = this.Team_total.toString();
+        else if(this.team_name=="team2")
+        document.getElementById("score2").innerHTML = this.Team_total.toString();
+        console.log(this.Team_total);
+        this.top_scorer.total=this.Team_total
+        console.log(this.top_scorer)
+
+    }
+
+
+
+
+}
+
+let Team1 = new Team([{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+{ player: [], total_run: 0 },
+
+],"team1")
+
+let Team2 = new Team([{ player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    { player: [], total_run: 0 },
+    
+    ],"team2")
+
+let team1_total=Team1.top_scorer;
+let team2_total=Team2.top_scorer;
+
+let maxTeam1=Team1.top_scorer;
+let maxTeam2=Team2.top_scorer;
+
+
+var generate_result=()=>{
+   let px=true;
+    console.log(team2_total.total);
+    if(Number(team1_total.total)>Number(team2_total.total)&&team1_total.total!=0 && team2_total.total!=0)
+    document.getElementById("won").innerHTML="MATCH WON BY TEAM1 ";
+    else if(Number(team1_total.total)<Number(team2_total.total)&&team1_total.total!=0 && team2_total.total!=0)
+    document.getElementById("won").innerHTML="MATCH WON BY TEAM2 ";
+    else if(Number(team1_total.total)==Number(team2_total.total)&&team1_total.total!=0)
+    document.getElementById("won").innerHTML="MATCH DRAW ";
+    else
+    {
+    document.getElementById("won").innerHTML="BOTH TEAM HAS NOT PLAYED ";
+    px=false;
+    }
+
+    if(!px)
+    document.getElementById("mom").innerHTML="";
+    else
+    {
+    if(Number(maxTeam1.run)>Number(maxTeam2.run))
+    {
+        document.getElementById("mom").innerHTML="Man of the Match :"
+                                                  + maxTeam1.Player_Id +
+                                                " Team 1 Score:"
+                                                + maxTeam1.run;
+    }
+    else if (Number(maxTeam1.run)<Number(maxTeam2.run))
+    {
+        document.getElementById("mom").innerHTML="Man of the Match : "+ maxTeam2.Player_Id+ " Team 2 Score:"+ maxTeam2.run;
+    }
+    else{
+        document.getElementById("mom").innerHTML="Man of the Match : "+ maxTeam1.Player_Id+ " Team 1 Score:"+maxTeam1.run;
+        document.getElementById("mom").innerHTML="Man of the Match : "+ maxTeam2.Player_Id+ " Team 1 Score:"+maxTeam2.run;
+    }
+}   
+
+}
+
+
+
